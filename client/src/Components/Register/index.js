8d6877bba0052315b5f0a90b3422d91a77e48149
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-10-09 11:21:51 -0700

    update component

---
+++ client/src/Components/Register/index.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import API from "../../utils/API";
+
+const Register = () => {
+  const [show, setShow] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+    console.log(formData);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+    // console.log({formData})
+    const { name, email, password } = formData;
+
+    if (name && email && password)
+      API.saveUser({
+        name,
+        points: 0,
+        email,
+        password,
+        act: []
+      })
+        .then(res => {
+          // const password = formData.password
+          // const rounds = 10
+
+          var userId = res.data._id;
+
+          localStorage.setItem("userId", userId);
+        })
+        .catch(err => console.log(err));
+  };
+
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <a variant="primary" onClick={handleShow}>
+        Sign Up
+      </a>
+
+      
+          <form>
+            <div className="form-group">
+              <label for="Name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <label for="Email">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div class="form-group">
+              <label for="Password">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </div>
+          </form>
+        
+          <Button variant="primary" onClick={handleClose}>
+            Register Me!
+          </Button>
+       
+    </>
+  );
+};
+
+export default Register;
