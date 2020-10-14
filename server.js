fe5a6f615529dba4e4b0b559143b14d2cb2831b6
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-10-14 12:01:39 -0700

    connect to mongo

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +10,16 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
+
+///connection to mongo
+var database = "mongodb://localhost/chorez"
+
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect(database);
+  console.log("connected to mongo");
+}
 
 app.get("*", (req, res) => {
   res.sendFile('index.html', {
