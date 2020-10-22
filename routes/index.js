c2e82d5cda58fb9117ff7810fd5b75116be62a3d
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-10-22 09:31:44 -0700

    index

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./API");
+const authRoutes = require("./authRoutes");
+
+//localhost:3000/
+// API Routes
+router.use("/API", apiRoutes);
+
+//use auth routes
+
+// If no API routes are hit, send the React app
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
