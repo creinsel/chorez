1e5db80329736dc1b4991d317d35f945c72bb054
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-11-10 13:19:32 -0800

    state in nav

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 
 
 
 const Nav = () => {
 
+  const [reg, setReg]= useState(false);
+  const [log, setLog]= useState(false);
+  const [wel, setWel]= useState(true);
+
+  const showReg = () =>{
+    setReg(true);
+    setLog(false);
+    setWel(false);
+  };
+
+  const showLog = () =>{
+    setReg(false);
+    setLog(true);
+    setWel(false);
+  };
  
+  const showWel = () =>{
+    setReg(false);
+    setLog(false);
+    setWel(true);
+  };
  
     return (
         <>
         
       <nav className="navbar sticky-top navbar-expand-lg bg-danger navSt">
       <div className="logo">
-          <h3 className="gfont">Chorez</h3>
+          <h3 className="gfont" onClick = {showWel}>Chorez</h3>
         </div>
      
     
       <div className="collapse navbar-collapse nav-menu mr-auto justify-content-end">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <Button>Login</Button>
+            <Button onClick = {showLog}>Login</Button>
           </li>
           <li className="nav-item">
-            <Button>Register</Button>
+            <Button onClick = {showReg}>Register</Button>
           </li>
         </ul>
         
--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -3,25 +3,9 @@
   import Disclaimer from  '../../Components/Disclaimer';
   import Register from '../../Components/Register';
   import Login from '../../Components/Login';
-  import Nav from './Components/Nav';
+  import Nav from '../../Components/Nav';
 
 const Welcome = () =>{
-
-  const [reg, setReg]= useState(false);
-  const [log, setLog]= useState(false);
-  const [wel, setWel]= useState(true);
-
-  const showReg = () =>{
-    setReg(true);
-    setLog(false);
-    setWel(false);
-  };
-
-  const showLog = () =>{
-    setReg(false);
-    setLog(true);
-    setWel(false);
-  };
 
     return(
         <>
