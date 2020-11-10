e80349d8f0ded9e7fad9561156aa0cad2c7af94a
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-11-10 12:55:42 -0800

    welc state

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -1,10 +1,15 @@
-  import React from "react";
+  import React, { useState } from "react";
   import Description from  '../../Components/Description';
   import Disclaimer from  '../../Components/Disclaimer';
   import Register from '../../Components/Register';
   import Login from '../../Components/Login';
 
 const Welcome = () =>{
+
+  const [reg, setReg]= useState(false);
+  const [log, setLog]= useState(false);
+  const [wel, setWel]= useState(true);
+
     return(
         <>
         <Description/>
